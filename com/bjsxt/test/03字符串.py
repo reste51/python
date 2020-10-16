f195f1e3848eb332aa1338bcad9425373b4cd9a9
reste51 <61609268+reste51@users.noreply.github.com>
2020-10-16 14:57:16 +0800

    基本元素的操作

--- com/bjsxt/test/03字符串.py
+++ com/bjsxt/test/03字符串.py
@@ -32,3 +32,51 @@ print(mystr.find("abc"))  # 找不到会返回-1
 # 每个单词的首字母 大写
 print(mystr.title())
 print(mystr.upper())
+
+print('~'*100)
+
+'''
+查找字符串的唯一要素
+以下代码可用于查找字符串中所有的唯一要素。
+我们使用其属性，其中一套字符串中的所有要素都是唯一的。
+'''
+my_string = "aavvccccddddeee"
+# converting the string to a set, 去重每个字符
+temp_set = set(my_string)
+# stitching set into a string using join
+new_string = '.'.join(temp_set)
+print(new_string)
+
+print('.'*100)
+
+'''
+将字符串拆分成子字符串列表
+通过使用.split方法，可以将字符串分成子字符串列表。还可以将想拆分的分隔符作为参数传递。
+'''
+string_1 = "My name is Chaitanya Baweja"
+string_2 = "sample/ string 2"
+# default separator
+print(string_1.split(' '))
+# [ My , name , is , Chaitanya , Baweja ]
+# defining separator as /
+print(string_2.split( '/' ))
+# [ sample , string 2 ]
+
+'''
+检查唯一性
+以下函数将检查一个列表中的所有要素是否唯一。
+'''
+def unique(l):
+    if len(l)==len(set(l)):   # 每个元素 无重复
+        print("All elements are unique")
+    else:           # 存在重复数据
+        print("List has duplicates")
+unique([1,2,3,4])
+# All elements are unique
+unique([1,1,2,3])
+# List has duplicates
+
+
+
+
+
