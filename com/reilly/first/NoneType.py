952d51b9b868c86f90c45a2188e0efbc95eca7c4
reste51 <61609268+reste51@users.noreply.github.com>
2020-10-30 09:37:32 +0800

    书籍学习

--- com/reilly/first/NoneType.py
+++ com/reilly/first/NoneType.py
@@ -16,3 +16,16 @@ def add_maybe_multipy(a,b,c=None):
 
 ret = add_maybe_multipy(2,10,10)
 print(ret)
+
+print('*'*100)
+
+for i in range(4):
+    for j in range(4):
+        if(j>i):
+            break;
+        print(i,j)
+
+print( list(range(10)))
+print( list(range(0,20,2)))
+print( list(range(5,0,-1)))
+
