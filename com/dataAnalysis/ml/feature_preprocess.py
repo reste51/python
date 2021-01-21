65a9052d57bae00dc5efc62a7a40bb8e6d521c12
reste51 <502042437@qq.com>
2021-01-21 22:22:48 +0800

    数值类型的-特征抽取-标准化（方差-标准差）

--- com/dataAnalysis/ml/feature_preprocess.py
+++ com/dataAnalysis/ml/feature_preprocess.py
@@ -1,9 +1,9 @@
 """
 特征预处理
-    数值型数据_针对每列（特征）处理_ 标准缩放
+    数值型数据_标准缩放：
 
 1. 归一化
-    归一化首先在特征（维度）非常多的时候，可以防止某一维或某几维对数据影响过大，也是为了把不同来源的数据统一到一个参考区间下，这样比较起来才有意义，其次可以程序可以运行更快。
+    作用于每列特征， 首先在特征（维度）非常多的时候，可以防止某一维或某几维对数据影响过大，也是为了把不同来源的数据统一到一个参考区间下，这样比较起来才有意义，其次可以程序可以运行更快。
     受异常点 影响较大, 鲁棒性较差
 
 
@@ -13,9 +13,15 @@
 
   其中\muμ是样本的均值，\sigmaσ是样本的标准差，它们可以通过现有的样本进行估计，在已有的样本足够多的情况下比较稳定，适合嘈杂的数据场景
   由于是使用了mean的平均值， 因此受异常点 影响较小, 鲁棒性较好
-"""
 
-from sklearn.preprocessing import MinMaxScaler,StandardScaler
+ 3. 空置处理：
+    删除（不建议），  填充（建议 ，使用平均值/ 中位数 每列填补）
+    pandas 也可以处理
+
+
+"""
+import  numpy as np
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,Imputer
 
 def normalization():
     """
@@ -45,10 +51,34 @@ def standardlization():
     ret_arr = std_vector.fit_transform(data)  # numpy array of shape
     print(type(ret_arr), ret_arr,sep="\n")
 
+def nan_process():
+    """
+    空值处理 - 详情参考源码
+    0计算过程： 0为列， nan =  (1+8)/2 =4.5
+
+     missing_values:
+      For missing values encoded as np.nan, use the string value "NaN".  float类型
+
+     - If `axis=0`, then impute along columns.
+     - If `axis=1`, then impute along rows.
+
+    strategy
+     - If "mean", then replace missing values using the mean along
+      the axis.
+    - If "median", then replace missing values using the median along
+      the axis.
+    - If "most_frequent", then replace missing using the most frequent
+      value along the axis.
+    :return:
+    """
+    nan_process = Imputer(missing_values='NaN',strategy='mean',axis=0)
+    data = [[1, 2], [np.nan, 3], [8, 6]]  # np.nan 为float
+    print(nan_process.fit_transform(data))
 
 if __name__=='__main__':
     # normalization()
-    standardlization()
+    # standardlization()
+    nan_process()
 
 
 
