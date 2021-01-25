fc233d0852163ffb84972f8eb3e75d8012e49bb7
reste51 <61609268+reste51@users.noreply.github.com>
2021-01-25 17:45:01 +0800

    数据降维

--- com/dataAnalysis/ml/dimension_reduction/first.py
+++ com/dataAnalysis/ml/dimension_reduction/first.py
@@ -2,5 +2,15 @@
 数据降维：
     该维度是特征的数量， 降维则是  减少特征。
 
+    特征选择：
+        1. filter:  varianceFilter方差过滤( 过滤低于 指定方差值以下的特征， 删除相同值的特征)
+            包名: feature_selection
+        2. embed： 正则化、决策树
+
+    PCA_ principal component analysis 主成分分析：
+
+
+
+
 
 """
