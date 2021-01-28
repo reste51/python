b45a61c3a51d442c89e5da1590505df0c38783f7
reste51 <61609268+reste51@users.noreply.github.com>
2021-01-28 17:38:04 +0800

    机器学习的练习---

---
+++ com/dataAnalysis/ml/dimension_reduction/excise_market.py
@@ -0,0 +1,20 @@
+"""
+    用户类别的分析(用户喜好， 下次会购买哪些商品？)：
+    目标值-- >
+               类别
+    userId  1   土豪
+    userId  2   贫民
+
+    1. 合并所需表 为一张表  , pandas.merge( rel_id)
+    products.csv                         商品信息
+    order_products__prior.csv           订单与商品信息
+    orders.csv                          用户的订单信息
+    aisles.csv                          商品所属具体物品类别
+
+    2.
+
+
+
+"""
+
+
--- com/dataAnalysis/ml/dimension_reduction/first.py
+++ com/dataAnalysis/ml/dimension_reduction/first.py
@@ -4,12 +4,12 @@
 
     特征选择：
         1. filter:  varianceFilter方差过滤( 过滤低于 指定方差值以下的特征， 删除相同值的特征)
-            包名: feature_selection
+            包名: feature_selection   ; removes all low-variance features
 
-          PCA：principal component analysis 主成分分析( 应用场景少)：
+          PCA：principal component analysis 主成分分析( 应用场景少)： 降低维度，数据量尽量减少损失
              包名: decomposition  ; 特征数量达到 上百个时使用
               1.2.1 将高维数据  降低低维, 数据不会损失很多。
-              1.2.2 高维数据问题： 特征之间通常是相关的    - 22：31
+              1.2.2 高维数据问题： 特征之间通常是相关(通过缩放比例是重复的)
 
         2. embed： 正则化、决策树
 
@@ -21,6 +21,7 @@
 """
 
 from sklearn.feature_selection import VarianceThreshold
+from sklearn.decomposition import  PCA
 
 def varianceThreshold():
     """
@@ -31,8 +32,20 @@ def varianceThreshold():
     data = var.fit_transform([[0,2,0,3],[0,1,4,3],[0,1,1,3]])
     print(data)
 
+def pca():
+    """
+    主成分分析
+    n_components 特征信息损失量：
+        1. 小数( 0-1,  0% - 100%): 损失率， 常用 ( 0.9 - 0.95)
+        2. 整数:保留的特征个数; 一般不会用
+    :return:
+    """
+    pca = PCA(n_components=0.9)  # 原4个特征 减少为2, 损失量 10%
+    # pca = PCA(n_components=1)  # 原4个特征 减少为1
+    reduce_data = pca.fit_transform([[2,8,4,5],[6,3,0,8],[5,4,9,1]])
+    print(reduce_data)
 
 if __name__ =='__main__':
-    varianceThreshold()
-
+    # varianceThreshold()
+    pca()
 
