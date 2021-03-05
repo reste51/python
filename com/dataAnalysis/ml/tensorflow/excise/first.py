10fe4ac46ad674de89f7c66f57e14eca3cfa2115
reste51 <61609268+reste51@users.noreply.github.com>
2021-03-05 16:49:44 +0800

    tensorFlow

--- com/dataAnalysis/ml/tensorflow/excise/first.py
+++ com/dataAnalysis/ml/tensorflow/excise/first.py
@@ -12,10 +12,9 @@
 """
 import tensorflow as tf
 
-tf = tf.compat.v1
-
+# tf = tf.compat.v1
 # 开启版本v1 兼容
-tf.disable_eager_execution()
+# tf.disable_eager_execution()
 
 # a,b 为tensor 张量
 a = tf.constant(10)
@@ -33,15 +32,15 @@ sum2 = b + aa
 # 创建一张新的图,需要包含一组op和tensor,与graph 不是同一块的内存
 new_g = tf.Graph()
 with new_g.as_default():  # 切换上下文，使用新建的图
-    # 占位符 与 feed_dict 实时训练搭配使用. None代表不确定数量(样本数不确定)
-    plt = tf.placeholder(tf.float32, shape=(None, 2))  # 定义图结构的一部分
+    # 占位符 与 feed_dict 实时训练搭配使用. None代表不确定数量(样本数不确定),输出的是 ?
+    # shape: 0阶为(), 1阶(1,)
+    plt = tf.placeholder(tf.float32, shape=(1,))  # 定义图结构的一部分
     # c = tf.constant(11.0)
     # print(c.graph)
     with tf.Session() as s2:
         # 运行一个占位符，训练过程中才知道实际的数据（读取文件）
-        ret = s2.run(plt, feed_dict={plt: [[1, 2], [3, 4], [5, 6], [5, 6]]})
+        ret = s2.run(plt, feed_dict={plt: [1]})
         print(ret,plt)
-
 
 print('*' * 100)
 
@@ -51,8 +50,7 @@ graph = tf.get_default_graph()
 # 开启会话，运行某一个个图
 # with 开启session的上下文环境，结束后会默认调用 .close()函数
 # log_device_placement -> 输出后端执行图的设备名称
-with tf.compat.v1.Session(graph=graph,
-                          config=tf.ConfigProto(log_device_placement=True)) as session:
+with tf.Session(graph=graph, config=tf.ConfigProto(log_device_placement=True)) as session:
     # 注: 错误: is not an element of this graph.  sum1是定义在 默认图中的
     # 不能运行c，因为c是定义在 new_g的图里; session运行的是graph图； 可以通过graph参数指定运行的图
     # run 说明运行图中的数据(多个使用 [] 传递), run 和eval 都需要有session的上下文环境;
